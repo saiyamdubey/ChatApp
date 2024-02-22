f57163b3c3a6030376e26dff1a061fa4513c1bd9
saiyamdubey <saiyamdubey8787@gmail.com>
2024-02-22 18:50:05 +0530

    Added View Analytics using nextjs

--- src/components/ui/background-gradient-animation.tsx
+++ src/components/ui/background-gradient-animation.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { cn } from "../../../utils/cn";
+import { cn } from "@/../utils/cn";
 import { useEffect, useRef, useState } from "react";
 
 export const BackgroundGradientAnimation = ({
