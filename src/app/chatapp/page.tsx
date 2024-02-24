5be58003017430ca4992f66021cb3c9002625c3b
saiyamdubey <saiyamdubey8787@gmail.com>
2024-02-25 02:57:15 +0530

    chat the socket.io("") to work with the diffrent systems

--- src/app/chatapp/page.tsx
+++ src/app/chatapp/page.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000");
+const socket = io("https://connectit.vercel.app/");
 
 export default function Page() {
   const [messages, setMessages] = useState<string[]>([]);
