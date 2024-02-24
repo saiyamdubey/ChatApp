4e6a692e1d27398c379ec6c9e4aeb55c8e6f690d
saiyamdubey <saiyamdubey8787@gmail.com>
2024-02-25 02:36:14 +0530

    Frontend of the chatapp done not fully

--- src/app/chatapp/page.tsx
+++ src/app/chatapp/page.tsx
@@ -29,19 +29,27 @@ export default function Page() {
 
   return (
     <>
-      <div>
+      <h1 className=" font-extrabold text-7xl text-center text-violet-400 ">
+        Messages from Server:
+      </h1>
+      <div className="flex justify-center flex-col gap-9 items-center">
         <form onSubmit={handleSubmit}>
           <input
+            className="text-white text-xl border-2 border-x-white bg-black placeholder:text-gray-400 w-[20rem] rounded-lg h-[5rem] p-4 mt-10 focus:outline-none"
             type="text"
             value={send}
             onChange={(e) => setSend(e.target.value)}
             placeholder="Type your message"
           />
-          <button type="submit">Send</button>
+          <button
+            className="text-white text-xl border-y-rose-50 border-2 bg-black rounded-lg px-4 py-2 ml-2"
+            type="submit"
+          >
+            Send Message
+          </button>
         </form>
-        <div>
-          <h1>Messages from Server:</h1>
-          <ul>
+        <div className="">
+          <ul className="text-xl font-mono text-pink-500">
             {messages.map((message, index) => (
               <li key={index}>{message}</li>
             ))}
