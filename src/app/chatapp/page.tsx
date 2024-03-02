a20bb5869bbc45e11a6716235c2cdf9be4aee617
saiyamdubey <saiyamdubey8787@gmail.com>
2024-03-03 01:09:26 +0530

    styling added to globals

--- src/app/chatapp/page.tsx
+++ src/app/chatapp/page.tsx
@@ -72,7 +72,7 @@ export default function Page() {
     { username: string; message: string }[]
   >([]);
   const [send, setSend] = useState<string>("");
-  const [username, setUsername] = useState<string>(""); // New state for username
+  const [username, setUsername] = useState<string>("");
 
   useEffect(() => {
     socket.on(
@@ -97,34 +97,11 @@ export default function Page() {
 
   return (
     <>
-      <h1 className="font-extrabold font-mono text-7xl text-center text-violet-400">
-        Private Chatting Room
+      <h1 className="font-extrabold font-mono text-6xl mb-2 text-center text-violet-400">
+        Private Chat
       </h1>
-      <div className=" justify-center  gap-9 items-center">
-        <form onSubmit={handleSubmit}>
-          <input
-            className="text-white text-xl border-2 border-x-white bg-black placeholder:text-gray-400 w-[20rem] rounded-lg h-[5rem] p-4 mt-5 focus:outline-none"
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Your Username"
-          />
-          <input
-            className="text-white text-xl border-2 border-x-white bg-black placeholder:text-gray-400 w-[20rem] rounded-lg h-[5rem] p-4 mt-10 focus:outline-none"
-            type="text"
-            value={send}
-            onChange={(e) => setSend(e.target.value)}
-            placeholder="Type your message"
-          />
-
-          <button
-            className="text-white text-xl border-y-rose-50 border-2 bg-black rounded-lg px-4 py-2 ml-2"
-            type="submit"
-          >
-            Send Message
-          </button>
-        </form>
-        <div className="">
+      <div className="h-[80vh] flex ">
+        <div className="border-2 border-white w-11/12 h-[95%] flex">
           <ul className="text-xl font-mono text-pink-500">
             {messages.map((messageObj, index) => (
               <li
@@ -138,6 +115,31 @@ export default function Page() {
               </li>
             ))}
           </ul>
+        </div>
+        <div className="flex-row justify-center items-center  bg-orange-700">
+          <form onSubmit={handleSubmit}>
+            <input
+              className="text-white text-xl border-2 border-x-white bg-black placeholder:text-gray-400 w-[20rem] rounded-lg h-[5rem] p-4 mt-5 focus:outline-none"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Your Username"
+            />
+            <input
+              className="text-white text-xl border-2 border-x-white bg-black placeholder:text-gray-400 w-[20rem] rounded-lg h-[5rem] p-4 mt-10 focus:outline-none"
+              type="text"
+              value={send}
+              onChange={(e) => setSend(e.target.value)}
+              placeholder="Type your message"
+            />
+
+            <button
+              className="text-white text-xl border-y-rose-50 border-2 bg-black rounded-lg px-4 py-2 ml-2"
+              type="submit"
+            >
+              Send Message
+            </button>
+          </form>
         </div>
       </div>
     </>
