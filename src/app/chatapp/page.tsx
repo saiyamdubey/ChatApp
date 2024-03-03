468bd56348acdfc1f683b1a0868bf4b073a9af01
saiyamdubey <saiyamdubey8787@gmail.com>
2024-03-03 16:26:02 +0530

    use message object to store the information

--- src/app/chatapp/page.tsx
+++ src/app/chatapp/page.tsx
@@ -65,7 +65,8 @@
 import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("https://chatserver-q3gi.onrender.com/");
+// const socket = io("https://chatserver-q3gi.onrender.com/");
+const socket = io("http://localhost:4000/");
 
 export default function Page() {
   const [messages, setMessages] = useState<
@@ -100,46 +101,52 @@ export default function Page() {
       <h1 className="font-extrabold font-mono text-6xl mb-2 text-center text-violet-400">
         Private Chat
       </h1>
-      <div className="h-[80vh] flex ">
-        <div className="border-2 border-white w-11/12 h-[95%] flex">
-          <ul className="text-xl font-mono text-pink-500">
+      <div className="h-[80vh] flex">
+        <div className="border-2 border-white w-11/12 h-[100%] overflow-y-scroll">
+          <ul className="text-2xl font-mono ">
             {messages.map((messageObj, index) => (
               <li
                 key={index}
-                className={
-                  messageObj.username === username ? "text-right" : "text-left"
-                }
+                className={` m-4 p-7 text-xl flex justify-start items-center rounded-3xl bg-violet-900 ${
+                  index % 2 === 0 ? "justify-start" : "justify-end"
+                }`}
+                style={{ maxWidth: "45%" }}
               >
-                <span className="font-bold">{messageObj.username}: </span>
-                {messageObj.message}
+                <span className="font-bold text-center text-wrap ">
+                  {messageObj.username}:
+                </span>
+                <p className="inline text-wrap ">{messageObj.message}</p>
               </li>
             ))}
           </ul>
         </div>
-        <div className="flex-row justify-center items-center  bg-orange-700">
-          <form onSubmit={handleSubmit}>
-            <input
-              className="text-white text-xl border-2 border-x-white bg-black placeholder:text-gray-400 w-[20rem] rounded-lg h-[5rem] p-4 mt-5 focus:outline-none"
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              placeholder="Your Username"
-            />
-            <input
-              className="text-white text-xl border-2 border-x-white bg-black placeholder:text-gray-400 w-[20rem] rounded-lg h-[5rem] p-4 mt-10 focus:outline-none"
-              type="text"
-              value={send}
-              onChange={(e) => setSend(e.target.value)}
-              placeholder="Type your message"
-            />
 
-            <button
-              className="text-white text-xl border-y-rose-50 border-2 bg-black rounded-lg px-4 py-2 ml-2"
-              type="submit"
-            >
-              Send Message
-            </button>
-          </form>
+        <div className="flex justify-center items-center border-2 border-wihte bg-orange-700 pl-32 ">
+          <div className="flex justify-center items-center">
+            <form onSubmit={handleSubmit} className=" ">
+              <input
+                className="text-white text-xl border-2 border-x-white bg-black placeholder:text-gray-400 w-[25rem] rounded-3xl h-[5rem] p-4 mt-5 focus:outline-none"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Your Username"
+              />
+              <input
+                className="text-white text-xl border-2 border-x-white bg-black placeholder:text-gray-400 w-[25rem] rounded-3xl h-[5rem] p-4 mt-10 focus:outline-none"
+                type="text"
+                value={send}
+                onChange={(e) => setSend(e.target.value)}
+                placeholder="Type your message"
+              />
+
+              <button
+                className="text-white text-xl mx-auto border-y-rose-50 border-2 bg-black rounded-lg px-4 py-2 block mt-5"
+                type="submit"
+              >
+                Send Message
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </>
