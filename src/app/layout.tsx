320abb7730f53fb9b310c985e55236e9ed81ae70
saiyamdubey <saiyamdubey8787@gmail.com>
2024-03-04 23:38:32 +0530

    SpeedInsights Added to vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <Analytics />
+        <SpeedInsights/>
       </body>
     </html>
   );
