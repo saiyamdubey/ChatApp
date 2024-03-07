bf97cd50f8e8db762b04d04039bd8d1f8bbc6c18
saiyamdubey <saiyamdubey8787@gmail.com>
2024-03-07 23:32:01 +0530

    Update socket.io server URL

--- src/app/chatapp/page.tsx
+++ src/app/chatapp/page.tsx
@@ -3,15 +3,15 @@
 import React, { useEffect, useState, useRef } from "react";
 import { io } from "socket.io-client";
 
-// const socket = io("http://localhost:4000/");
-const socket = io("https://chatserver-q3gi.onrender.com/");
+const socket = io("http://localhost:4000/");
+// const socket = io("https://chatserver-q3gi.onrender.com/");
 
 interface Message {
   username: string;
   message: string;
   sentByCurrentUser?: boolean;
   timestamp: string;
-  room: string; 
+  room: string;
 }
 
 export default function Page() {
@@ -19,10 +19,13 @@ export default function Page() {
   const [send, setSend] = useState("");
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
+  const [online, setonline] = useState(0);
   const chatContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     socket.on("message", (message) => {
+      console.log(online);
+      // setonline(socket.);
       setMessages((prevMessages) => [...prevMessages, message]);
     });
 
@@ -33,7 +36,8 @@ export default function Page() {
 
   useEffect(() => {
     if (chatContainerRef.current) {
-      chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+      chatContainerRef.current.scrollTop =
+        chatContainerRef.current.scrollHeight;
     }
   }, [messages]);
 
@@ -50,7 +54,10 @@ export default function Page() {
       const timestamp = getCurrentTime();
       const message: Message = { username, message: send, timestamp, room };
       socket.emit("message", message);
-      setMessages((prevMessages) => [...prevMessages, { ...message, sentByCurrentUser: true }]);
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { ...message, sentByCurrentUser: true },
+      ]);
       setSend("");
     }
   }
@@ -64,73 +71,77 @@ export default function Page() {
 
   return (
     <>
-      <h1 className="font-extrabold font-mono text-6xl mb-2 text-center text-violet-400">
-        Global Private Chat
-      </h1>
-      <div className="h-[80vh] flex">
-        <div
-          ref={chatContainerRef}
-          className={`border-2 border-white w-11/12 h-[100%] overflow-y-scroll`}
-        >
-          <ul className="text-2xl font-mono ">
-            {messages.map((messageObj, index) => (
-              <li
-                key={index}
-                className={`m-4 p-7 text-xl flex items-center rounded-3xl ${
-                  messageObj.sentByCurrentUser ? "bg-green-500 justify-end" : "bg-orange-600 justify-start"
-                }`}
-              >
-                <span className="font-bold text-center text-wrap ">
-                  {messageObj.username} ({messageObj.timestamp}):
-                </span>
-                <p className="inline text-wrap">{messageObj.message}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
+      <div>
+        <h1 className="font-extrabold font-mono text-6xl mb-2 text-center text-violet-400">
+          Global Private Chat {online}
+        </h1>
+        <div className="flex h-[80vh] ">
+          <div
+            ref={chatContainerRef}
+            className={`border-2 border-white w-11/12 h-[100%] overflow-y-scroll `}
+          >
+            <ul className="text-2xl font-mono ">
+              {messages.map((messageObj, index) => (
+                <li
+                  key={index}
+                  className={`m-4 p-7 text-xl flex items-center rounded-3xl ${
+                    messageObj.sentByCurrentUser
+                      ? "bg-green-500 justify-end"
+                      : "bg-orange-600 justify-start"
+                  }`}
+                >
+                  <span className="font-bold text-center text-wrap ">
+                    {messageObj.username} ({messageObj.timestamp}):
+                  </span>
+                  <p className="inline text-wrap">{messageObj.message}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
 
-        <div className="flex justify-center items-center border-2 border-wihte  pl-32 ">
-          <div className="flex justify-center items-center">
-            <form onSubmit={handleJoinRoom} className=" ">
-              <input
-                className="text-white text-xl border-2 border-x-white bg-black placeholder:text-gray-400 w-[25rem] rounded-3xl h-[5rem] p-4 mt-5 focus:outline-none"
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                placeholder="Your Username"
-              />
-              <input
-                className="text-white text-xl border-2 border-x-white bg-black placeholder:text-gray-400 w-[25rem] rounded-3xl h-[5rem] p-4 mt-5 focus:outline-none"
-                type="text"
-                value={room}
-                onChange={(e) => setRoom(e.target.value)}
-                placeholder="Room Name"
-              />
+          <div className="flex  justify-center items-center border-2 border-wihte  pl-32 ">
+            <div className="flex flex-col justify-center items-center">
+              <form onSubmit={handleJoinRoom} className=" items-center flex">
+                <input
+                  className="text-white text-xl border-2 border-x-white bg-black placeholder:text-gray-400 w-[25rem] rounded-3xl h-[5rem] p-4 mt-5 focus:outline-none"
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  placeholder="Your Username"
+                />
+                <input
+                  className="text-white text-xl border-2 border-x-white bg-black placeholder:text-gray-400 w-[25rem] rounded-3xl h-[5rem] p-4 mt-5 focus:outline-none"
+                  type="text"
+                  value={room}
+                  onChange={(e) => setRoom(e.target.value)}
+                  placeholder="Room Name"
+                />
 
-              <button
-                className="text-white text-xl mx-auto border-y-rose-50 border-2 bg-black rounded-lg px-4 py-2 block mt-5"
-                type="submit"
-              >
-                Join Room
-              </button>
-            </form>
+                <button
+                  className="text-white text-xl mx-auto border-y-rose-50 border-2 bg-black rounded-lg px-4 py-2 block mt-5"
+                  type="submit"
+                >
+                  Join Room
+                </button>
+              </form>
 
-            <form onSubmit={handleSubmit} className=" ">
-              <input
-                className="text-white text-xl border-2 border-x-white bg-black placeholder:text-gray-400 w-[25rem] rounded-3xl h-[5rem] p-4 mt-5 focus:outline-none"
-                type="text"
-                value={send}
-                onChange={(e) => setSend(e.target.value)}
-                placeholder="Type your message"
-              />
+              <form onSubmit={handleSubmit} className="">
+                <input
+                  className="text-white text-xl border-2 border-x-white bg-black placeholder:text-gray-400 w-[25rem] rounded-3xl h-[5rem] p-4 mt-5 focus:outline-none"
+                  type="text"
+                  value={send}
+                  onChange={(e) => setSend(e.target.value)}
+                  placeholder="Type your message"
+                />
 
-              <button
-                className="text-white text-xl mx-auto border-y-rose-50 border-2 bg-black rounded-lg px-4 py-2 block mt-5"
-                type="submit"
-              >
-                Send Message
-              </button>
-            </form>
+                <button
+                  className="text-white text-xl mx-auto border-y-rose-50 border-2 bg-black rounded-lg px-4 py-2 block mt-5"
+                  type="submit"
+                >
+                  Send Message
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
