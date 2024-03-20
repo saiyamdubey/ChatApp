7e8759d5823d57be9f57c6990a709eea7ecad8e4
saiyamdubey <saiyamdubey8787@gmail.com>
2024-03-21 02:47:03 +0530

    Update the link in the button from "/fileshare" to "/videoconferencing".

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -43,7 +43,7 @@ export default function Cards({}: Props) {
               connections, by know that you want to share your private video to
               the other users.
             </p>
-            <Link href="/fileshare" className="mx-auto cursor-pointer z-50">
+            <Link href="/videoconferencing" className="mx-auto cursor-pointer z-50">
               <button className="inline-flex sm:px-4 mx-auto h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 cursor-pointer">
                 Start Sharing
               </button>
