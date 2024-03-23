be7c206d6911fdf6dcc1f77b51f8f9559cb48fa0
saiyamdubey <saiyamdubey8787@gmail.com>
2024-03-24 01:19:18 +0530

    Added video conferencing features using webrtc and zegoclound for the single roomid

--- src/app/videoconferencing/page.jsx
+++ src/app/videoconferencing/page.jsx
@@ -1,8 +1,85 @@
 "use client";
 
+import React, { useEffect } from "react";
+
+function randomID(len) {
+  let result = "";
+  if (result) return result;
+  var chars = "12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP";
+  var maxPos = chars.length;
+  for (var i = 0; i < len; i++) {
+    result += chars.charAt(Math.floor(Math.random() * maxPos));
+  }
+  return result;
+}
+
+// export function getUrlParams(url = window.location.href) {
+//   let urlStr = url.split("?")[1];
+//   // return new URLSearchParams(urlStr);
+// }
+
+export default function Page() {
+  const roomID = "ram" ;
+
+  useEffect(() => {
+    const myMeeting = async (element) => {
+      if (typeof window === "undefined") return;
+
+      const { ZegoUIKitPrebuilt } = await import(
+        "@zegocloud/zego-uikit-prebuilt"
+      );
+
+      const appID = 1951306805;
+      const serverSecret = "7631914ccc8f251898066b596856c9e8";
+      const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+        appID,
+        serverSecret,
+        roomID,
+        randomID(5),
+        randomID(5)
+      );
+
+      const zp = ZegoUIKitPrebuilt.create(kitToken);
+
+      zp.joinRoom({
+        container: element,
+
+        sharedLinks: [
+          {
+            name: "Sharable link",
+            url:
+              window.location.protocol +
+              "//" +
+              window.location.host +
+              window.location.pathname +
+              "?roomID=" +
+              roomID,
+          },
+        ],
+        scenario: {
+          mode: ZegoUIKitPrebuilt.VideoConference,
+        },
+      });
+    };
+
+    const element = document.querySelector(".myCallContainer");
+    if (element) {
+      myMeeting(element);
+    }
+  }, [roomID]);
+
+  return <div className="myCallContainer w-dvw h-dvh"></div>;
+}
+
+
+
+
+
+
+
 // import React, { useEffect } from "react";
 
-// function randomID(len: any) {
+// function randomID(len) {
 //   let result = "";
 //   if (result) return result;
 //   var chars = "12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP";
@@ -13,16 +90,21 @@
 //   return result;
 // }
 
-// export function getUrlParams(url = window.location.href) {
+// export function getUrlParams(url) {
+//   if (typeof window === 'undefined') {
+//     return null; // or handle differently based on your use case
+//   }
+
+//   url = url || window.location.href;
 //   let urlStr = url.split("?")[1];
 //   return new URLSearchParams(urlStr);
 // }
 
 // export default function App() {
-//   const roomID = getUrlParams().get("roomID") || randomID(25);
+//   const roomID = typeof window !== 'undefined' ? getUrlParams("www.google.com").get("roomID") || randomID(25) : null;
 
 //   useEffect(() => {
-//     const myMeeting = async (element: any) => {
+//     const myMeeting = async (element) => {
 //       if (typeof window === "undefined") return;
 
 //       const { ZegoUIKitPrebuilt } = await import(
@@ -71,9 +153,16 @@
 //   return <div className="myCallContainer w-dvw h-dvh"></div>;
 // }
 
+
+
+
+
+
+
+
 // import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 
-// function randomID(len: any) {
+// function randomID(len) {
 //   let result = "";
 //   if (result) return result;
 //   var chars = "12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP",
@@ -87,15 +176,15 @@
 // }
 
 // export function getUrlParams(
-//   url: string = window.location.href
-// ): URLSearchParams {
+//   url = window.location.href
+// ) {
 //   let urlStr = url.split("?")[1];
 //   return new URLSearchParams(urlStr);
 // }
 
 // export default function App() {
 //   const roomID = getUrlParams().get("roomID") || randomID(5);
-//   let myMeeting = async (element: HTMLDivElement) => {
+//   let myMeeting = async (element) => {
 //     const appID = 1951306805;
 //     const serverSecret = "7631914ccc8f251898066b596856c9e8";
 //     const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
@@ -134,12 +223,14 @@
 //     </>
 //   );
 // }
-import React from 'react'
 
-type Props = {}
 
-export default function page({}: Props) {
-  return (
-    <div>page</div>
-  )
-}
+// import React from 'react'
+
+// type Props = {}
+
+// export default function page({}: Props) {
+//   return (
+//     <div>page</div>
+//   )
+// }
