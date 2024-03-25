464cb73604fa67a865ae985f790b7cb115f1fb1a
saiyamdubey <saiyamdubey8787@gmail.com>
2024-03-25 17:49:28 +0530

    Change room-related text to video/conference-related text in videoconferencing page; update background gradients
    The diff includes the following changes:
    
    * In the "Join Room" section, the heading text is changed from "Join Room" to "Join Video".
    * In the "Create Room" section, the heading text is changed from "Create Room" to "Create Conference".
    * Background gradients for both sections are updated with new fields "from-purple-600" and "from-orange-800" respectively.

--- src/app/videoconferencing/page.tsx
+++ src/app/videoconferencing/page.tsx
@@ -109,11 +109,11 @@ function Room() {
     <>
       <div className="flex sm:flex-col justify-center items-center gap-[15rem] sm:gap-[8rem] rounded-3xl bg-transparent w-[100%] h-dvh flex-row overflow-hidden">
         <div className="relative max-w-sm w-[50rem] sm:w-[80%] h-[15rem] md:h-2/2  ">
-          <div className="absolute inset-0 h-full w-[30rem] sm:w-[100%] bg-gradient-to-r from-transparent to-blue-600 transform scale-[1.20] rounded-full blur-3xl flex justify-center items-center" />
+          <div className="absolute inset-0 h-full w-[30rem] sm:w-[100%] bg-gradient-to-r from-transparent to-purple-600 transform scale-[1.20] rounded-full blur-3xl flex justify-center items-center" />
           <div className="relative h-full w-[30rem] sm:w-[100%]  border border-white px-8 sm:py-6 overflow-hidden rounded-2xl shadow-2xl flex justify-center items-center">
             <div className="join flex flex-col justify-center items-center text-center">
               <h1 className="font-extrabold font-mono text-4xl sm:text-xl mb-4 text-center text-violet-100">
-                Join Room
+                Join Video
               </h1>
               <input
                 className=" text-white text-lg w-[150%] bg-transparent border-2 mb-4 border-white focus:outline-none sm:h-10 sm:w-[110%] placeholder-gray-400 py-2 px-3 sm:rounded-3xl sm:pl-2 sm:p-1 rounded-3xl"
@@ -145,11 +145,11 @@ function Room() {
           </div>
         </div>
         <div className="relative max-w-sm w-[50rem] sm:w-[80%] h-[15rem] md:h-2/2 ">
-          <div className="absolute inset-0 h-full w-[30rem] sm:w-[100%] bg-gradient-to-r from-pink-800 to-transparent-500 transform scale-[1.20] rounded-full blur-3xl flex justify-center items-center" />
+          <div className="absolute inset-0 h-full w-[30rem] sm:w-[100%] bg-gradient-to-r from-orange-800 to-transparent-500 transform scale-[1.20] rounded-full blur-3xl flex justify-center items-center" />
           <div className="relative h-full w-[30rem] sm:w-[100%] border-2 border-white px-8 sm:py-6 overflow-hidden rounded-2xl shadow-2xl flex justify-center items-center">
             <div className="join flex flex-col justify-center items-center text-center">
               <h1 className="font-extrabold font-mono text-4xl sm:text-xl mb-4 text-center text-violet-100">
-                Create Room
+                Create Conference
               </h1>
               <button
                 className="visible sm:hidden text-white p-4 text-lg sm:text-[18px] font-extrabold bg-transparent border-2 border-white hover:bg-black hover:blue-600 hover:text-white rounded-full py-2 sm:rounded-md sm:w-20 sm:h-10 sm:text-white mt-4"
